897134c0ee7f6b57d76fffbd0317fe1c9e42af61
Bryce Cindrich <bryce.cindrich@gmail.com>
2021-02-15 19:57:27 -0800

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BC_IS413_Assignment5.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public String Title { get; set; }
+        [Required]
+        public String Author { get; set; }
+        [Required]
+        public String Publisher { get; set; }
+        [Required]
+        [RegularExpression("^(?:ISBN(?:-13)?:?\\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\\ ]){4})[-\\ 0-9]{17}$)97[89][-\\ ]?[0-9]{1,5}[-\\ ]?[0-9]+[-\\ ]?[0-9]+[-\\ ]?[0-9]$", ErrorMessage = "Must be a valid ISBN number in format and 10 or 13 digits")]
+        public String ISBN { get; set; }
+        [Required]
+        public String Classification { get; set; }
+        [Required]
+        public String Category { get; set; }
+        [Required]
+        [DataType(DataType.Currency, ErrorMessage = "Must be valid monetary value")]
+        public int Price { get; set; }
+    }
+}
